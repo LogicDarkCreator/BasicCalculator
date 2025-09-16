2d3b52db814314b2bd23e6cb9ad0d39794f8cfd5
JSD Dev Portal <logicdarkcreator@gmail.com>
2025-09-16 22:00:53 +0300

    Add files via upload

---
+++ configs/screen.py
@@ -0,0 +1,10 @@
+size = "268x288"
+title = "Calculator"
+font, state1, state0 = "Arial 15 bold", "readonly", "normal"
+r_width, r_height = 0, 0
+
+# Grid
+row, column, columnspan, sticky = 0, 0, 4, "nsew"
+
+# Input
+inputWidth = 24
